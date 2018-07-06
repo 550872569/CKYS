844973d00c2dbe6ed37cc24e68fd3f5115443f2f
YY <550872569@qq.com>
2018-07-06 17:29:26 +0800

    CKYSBCExcellentCourseViewController debug UI

--- CKYS/CKYS/BusinessCollege/New Group/ExcellentCourse/CKYSBCExcellentCourseViewController.m
+++ CKYS/CKYS/BusinessCollege/New Group/ExcellentCourse/CKYSBCExcellentCourseViewController.m
@@ -9,9 +9,12 @@
 #import "CKYSBCExcellentCourseViewController.h"
 #import "UIViewController+SearchItem.h"
 #import "CKYSBCSearchItem.h"
+#import "CKYSExcellentCourseContainerView.h"
+#import "CKYSScreenConst.h"
+#import "CKYSExcellentCourseContentCollectionView.h"
 
 @interface CKYSBCExcellentCourseViewController ()
-
+@property (nonatomic, strong) CKYSExcellentCourseContainerView *containerView;
 @end
 
 @implementation CKYSBCExcellentCourseViewController
@@ -21,6 +24,18 @@
     self.title = @"精品课程";
     self.view.backgroundColor = [UIColor whiteColor];
     [self initSearchItem];
+    [self initContainerView];
+    [self initContentView];
+}
+
+- (void)initContainerView {
+    _containerView = [[CKYSExcellentCourseContainerView alloc] initWithFrame:CGRectMake(0, SCREEN_STATUS_AND_NAVIGATION_HEIGHT, SCREEN_WIDTH, CKYS_EC_CONTAINER_HEIGHT)];
+    _containerView.backgroundColor = [UIColor yellowColor];
+    [self.view addSubview:_containerView];
+}
+- (void)initContentView {
+    CKYSExcellentCourseContentCollectionView *contentView  = [[CKYSExcellentCourseContentCollectionView alloc] initWithFrame:CGRectMake(0, SCREEN_STATUS_AND_NAVIGATION_HEIGHT+CKYS_EC_CONTAINER_HEIGHT, SCREEN_WIDTH, SCREEN_HEIGHT-SCREEN_STATUS_AND_NAVIGATION_HEIGHT-CKYS_EC_CONTAINER_HEIGHT-SCREEN_TAB_BAR_HEIGHT)];
+    [self.view addSubview:contentView];
 }
 
 - (void)initSearchItem {
